2c142d78c8248c0ac3aa59d84d8af86a6bd57d83
huh <miraebiz001@gmail.com>
2022-06-08 11:09:20 +0900

    ✨Styled component test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,26 @@ import type { NextPage } from "next";
 import styled from "styled-components";
 import { MainLayout } from "@/components";
 import { theme } from "@/style";
+import { Test } from "@/components";
 
 const Home: NextPage = () => {
   return (
     <MainLayout title="test">
-      <Main>안녕</Main>
+      <Main>
+        안녕
+        <div>What!!</div>
+        <Test />
+      </Main>
     </MainLayout>
   );
 };
 
 const Main = styled.div`
   background: ${theme.colors.Gray100};
+  color: black;
+  @media (max-width: 1000px) {
+    color: white;
+  }
 `;
 
 export default Home;
---
+++ src/components/common/Test.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Test = () => {
+  return <Text>HAHA</Text>;
+};
+
+const Text = styled.div`
+  display: block;
+`;
