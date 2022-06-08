b593e78f3c11142efc446f435a87a70ac747e56a
Jamie <58623333+Jamie-7@users.noreply.github.com>
2022-06-08 11:10:33 +0900

    Feature/issue 1 Merge (#4)
    * 🐛 fixed Warning React array unique "key" error
    
    * 🎨 Styled compenent  Nest 문법과 다른 Component 사용가능
    
    Co-authored-by: huh <miraebiz001@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,50 @@
 import type { NextPage } from "next";
 import styled from "styled-components";
-import { MainLayout } from "@/components";
+import { Button, MainLayout } from "@/components";
 import { theme } from "@/style";
 
 const Home: NextPage = () => {
   return (
     <MainLayout title="test">
-      <Main>안녕</Main>
+      <Main>
+        <Logo>Welcome</Logo>
+        <Form>
+          <Input type="text" name="id"></Input>
+          <Input type="password" name="pw"></Input>
+          <Button />
+        </Form>
+      </Main>
     </MainLayout>
   );
 };
 
 const Main = styled.div`
-  background: ${theme.colors.Gray100};
+  height: 100vh;
+`;
+
+const Form = styled.form`
+  width: 300px;
+  margin: auto;
+`;
+
+const Logo = styled.div`
+  font-size: 50px;
+  text-align: center;
+  margin: 200px auto 50px;
+  &:hover {
+    + ${Form} {
+      background: ${theme.colors.Gray100};
+    }
+  }
+`;
+
+const Input = styled.input`
+  text-align: center;
+  width: 200px;
+  padding: none;
+  margin: 10px auto;
+  display: block;
+  font-family: auto;
 `;
 
 export default Home;
